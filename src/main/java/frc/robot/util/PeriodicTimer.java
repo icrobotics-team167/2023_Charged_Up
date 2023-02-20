472a45da064c89e2098458ad02a6c59f2b51f15c
TheComputer314 <thecomputer314@gmail.com>
2023-02-20 12:35:09 -0600

    Code comments in PeriodicTimer

--- src/main/java/frc/robot/util/PeriodicTimer.java
+++ src/main/java/frc/robot/util/PeriodicTimer.java
@@ -7,23 +7,45 @@ public class PeriodicTimer {
     private double startTime;
     private boolean hasElapsedOnceFlag = false;
 
+    /**
+     * Constructs a new timer for use in periodic functions.
+     */
     public PeriodicTimer() {
         reset();
     }
 
+    /**
+     * Resets the timer.
+     */
     public void reset() {
         startTime = Timer.getFPGATimestamp();
         hasElapsedOnceFlag = false;
     }
 
+    /**
+     * Checks if a certain amount of time has passed
+     * 
+     * @param seconds The amount of time to check for
+     * @return Whether or not that amount of time has passed
+     */
     public boolean hasElapsed(double seconds) {
         return get() >= seconds;
     }
 
+    /**
+     * Gets the current time value, relative to when the timer was last reset.
+     * 
+     * @return The time value.
+     */
     public double get() {
         return Timer.getFPGATimestamp() - startTime;
     }
 
+    /**
+     * Gets the absolute time value, relative to when the RoboRIO was powered on.
+     * 
+     * @return The absolute time value.
+     */
     public double getAbsoluteTime() {
         return Timer.getFPGATimestamp();
     }
