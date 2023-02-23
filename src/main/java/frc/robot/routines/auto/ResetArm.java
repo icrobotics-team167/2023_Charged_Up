0230d4e8c3e0df566c67b692b634acbc7e163163
TheComputer <thecomputer314@gmail.com>
2023-02-22 21:05:37 -0600

    Create ResetArm.java

---
+++ src/main/java/frc/robot/routines/auto/ResetArm.java
@@ -0,0 +1,31 @@
+package frc.robot.routines.auto;
+
+import frc.robot.routines.Action;
+
+public class ResetArm extends Action {
+
+    @Override
+    public void init() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'init'");
+    }
+
+    @Override
+    public void periodic() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'periodic'");
+    }
+
+    @Override
+    public boolean isDone() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isDone'");
+    }
+
+    @Override
+    public void done() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'done'");
+    }
+
+}
