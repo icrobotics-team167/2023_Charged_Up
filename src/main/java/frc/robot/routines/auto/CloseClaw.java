07cc7c9defd14a35f079ba04c5c6972c9395dc4f
jeli26 <122842832+jeli26@users.noreply.github.com>
2023-02-24 21:53:00 -0600

    Made (bad) comments for my new classes

--- src/main/java/frc/robot/routines/auto/CloseClaw.java
+++ src/main/java/frc/robot/routines/auto/CloseClaw.java
@@ -4,6 +4,11 @@ import frc.robot.routines.Action;
 import frc.robot.subsystems.Subsystems;
 import frc.robot.util.PeriodicTimer;
 
+/**
+ * Closes the claw
+ * Constructor and init set up the timer
+ * The only done condition is if the timer has been running for 0.25 seconds
+ */
 public class CloseClaw extends Action {
 
     private final double WAIT_TIME = 0.25;
--- src/main/java/frc/robot/routines/auto/OpenClaw.java
+++ src/main/java/frc/robot/routines/auto/OpenClaw.java
@@ -4,6 +4,11 @@ import frc.robot.routines.Action;
 import frc.robot.subsystems.Subsystems;
 import frc.robot.util.PeriodicTimer;
 
+/**
+ * Opens the claw
+ * Constructor and init set up the timer
+ * The only done condition is if the timer has been running for 0.25 seconds
+ */
 public class OpenClaw extends Action {
 
     private final double WAIT_TIME = 0.25;
