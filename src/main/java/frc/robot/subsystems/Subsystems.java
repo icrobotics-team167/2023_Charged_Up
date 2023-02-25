c7c7447ab126739306e63ddfa52d85bdc4eb9fe3
TheComputer <thecomputer314@gmail.com>
2023-02-24 20:35:56 -0600

    Create turret inside Subsystems.java

--- src/main/java/frc/robot/subsystems/Subsystems.java
+++ src/main/java/frc/robot/subsystems/Subsystems.java
@@ -2,13 +2,16 @@ package frc.robot.subsystems;
 
 import frc.robot.subsystems.drive.SparkTankDriveBase;
 import frc.robot.subsystems.drive.TankDriveBase;
+import frc.robot.subsystems.turret.Turret;
 
 public class Subsystems {
 
     public static final TankDriveBase driveBase;
+    public static final Turret turret;
 
     static {
         driveBase = SparkTankDriveBase.getInstance();
+        turret = Turret.getinstance();
     }
 
     public static void setInitialStates() {
