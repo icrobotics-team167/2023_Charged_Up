023f7842bbbf795f81260268b9f0671938f5e432
TheComputer314 <thecomputer314@gmail.com>
2023-03-02 18:15:46 -0600

    Update turret positions

--- src/main/java/frc/robot/subsystems/turret/TurretPosition.java
+++ src/main/java/frc/robot/subsystems/turret/TurretPosition.java
@@ -14,8 +14,8 @@ public record TurretPosition(double pivotAngle, double swivelAngle, double exten
     }
 
     public static final TurretPosition INITIAL = new TurretPosition(60, 0, 3.5);
-    public static final TurretPosition HIGH_GOAL = new TurretPosition(37.7, 0, 39.6);
+    public static final TurretPosition HIGH_GOAL = new TurretPosition(35, 0, 39.6);
     public static final TurretPosition MID_GOAL = new TurretPosition(36, 0, 20.1);
     public static final TurretPosition INTAKE = new TurretPosition(-33, 0, 8);
-    public static final TurretPosition PLAYER_STATION = new TurretPosition(0, 0, 3.5);
+    public static final TurretPosition PLAYER_STATION = new TurretPosition(36, 0, 7); // TODO: Measure angles
 }
