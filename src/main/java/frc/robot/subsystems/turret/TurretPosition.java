61f0066c93d64e510101a0a78db37b0f2212b30f
cm167 <markovichc167@gmail.com>
2023-03-07 09:01:53 -0600

    Update TurretPosition.java
    Added automatic turret positions for the 3 high and 3 mid positions reachable by our arm when centered in any grid column.

--- src/main/java/frc/robot/subsystems/turret/TurretPosition.java
+++ src/main/java/frc/robot/subsystems/turret/TurretPosition.java
@@ -12,14 +12,30 @@ public record TurretPosition(double pivotAngle, double swivelAngle, double exten
     public TurretPosition withExtension(double extension) {
         return new TurretPosition(this.pivotAngle, this.swivelAngle, extension);
     }
-
-    public static final TurretPosition INITIAL = new TurretPosition(60, 0, 3.5);
-    public static final TurretPosition HIGH_GOAL_CENTER = new TurretPosition(35, 0, 39.6);
-    public static final TurretPosition HIGH_GOAL_CUBE_BLUE = HIGH_GOAL_CENTER.withSwivel(9.6);
-    public static final TurretPosition HIGH_GOAL_CUBE_RED = HIGH_GOAL_CENTER.withSwivel(-9.6);
-    public static final TurretPosition HIGH_GOAL_CONE_BLUE = HIGH_GOAL_CENTER.withSwivel(-12.5);
-    public static final TurretPosition HIGH_GOAL_CONE_RED = HIGH_GOAL_CENTER.withSwivel(12.5);
-    public static final TurretPosition MID_GOAL = new TurretPosition(36, 0, 20.1);
-    public static final TurretPosition INTAKE = new TurretPosition(-33, 0, 8);
-    public static final TurretPosition PLAYER_STATION = new TurretPosition(50, 0, 7); // TODO: Measure angles
+    
+    //commonly used positions
+    public static final TurretPosition INITIAL           = new TurretPosition(60, 0, 3.5);
+    public static final TurretPosition MID_GOAL          = new TurretPosition(36, 0, 20.1);
+    public static final TurretPosition HIGH_GOAL_CENTER  = new TurretPosition(35,   0, 39.6);
+    public static final TurretPosition INTAKE            = new TurretPosition(-33, 0, 8);
+    public static final TurretPosition PLAYER_STATION    = new TurretPosition(50, 0, 7); // TODO: Measure angles
+    
+    // 6 positions for single grid section PLEASE TEST THESE
+    public static final TurretPosition HIGH_LEFT  = new TurretPosition(35, -20, 42);
+    public static final TurretPosition HIGH_MID   = new TurretPosition(35,   0, 40);
+    public static final TurretPosition HIGH_RIGHT = new TurretPosition(35,  20, 42);
+    public static final TurretPosition MID_LEFT   = new TurretPosition(35, -28, 28);
+    public static final TurretPosition MID_MID    = new TurretPosition(35,   0, 21);
+    public static final TurretPosition MID_RIGHT  = new TurretPosition(35,  28, 28);
+    
+    //max extension can reach 2 more mid scoring positions PLEASE TEST THESE TOO (LOW PRIORITY)
+    public static final TurretPosition FAR_ MID_LEFT   = new TurretPosition(35, -46, 28);
+    public static final TurretPosition FAR_ MID_RIGHT  = new TurretPosition(35,  46, 28);
+    
+    //positions for auto only
+    public static final TurretPosition HIGH_GOAL_CUBE_BLUE = HIGH_GOAL_CENTER.withSwivel( 9.6);  // positioning for auto
+    public static final TurretPosition HIGH_GOAL_CUBE_RED  = HIGH_GOAL_CENTER.withSwivel(-9.6);  // positioning for auto
+    public static final TurretPosition HIGH_GOAL_CONE_BLUE = HIGH_GOAL_CENTER.withSwivel(-12.5); // positioning for auto
+    public static final TurretPosition HIGH_GOAL_CONE_RED  = HIGH_GOAL_CENTER.withSwivel( 12.5); // positioning for auto
+    
 }
