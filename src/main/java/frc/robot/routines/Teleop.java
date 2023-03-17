a6ee844acd6dc99c7add170f22ddc5e6deee79c0
TheComputer314 <thecomputer314@gmail.com>
2023-03-16 20:58:19 -0500

    Re-added the ability to toggle the limelight, made it possible to push the button in order to get the claw moving instead of holding it
    Add a deadzone or at least like a sensitivity threshold for the joystick.

--- src/main/java/frc/robot/routines/Teleop.java
+++ src/main/java/frc/robot/routines/Teleop.java
@@ -50,67 +50,71 @@ public class Teleop {
                     controls.getArcadeWheel());
         }
 
-        turret.setSlowMode(controls.doSlowTurret());
-
-        if(targetState!=null) {
-            turret.moveTo(targetState);
+        if (controls.toggleLimelight()) {
+            limeLight.toggleMode();
         }
+        // turret.setSlowMode(controls.doSlowTurret());
+
+        // Code so that u only have to press one button and it will automatically go to it. Do not commit until tested
 
         if (controls.doResetTurret()) {
-            // claw.closeClaw();
-            turret.moveTo(TurretPosition.INITIAL);
+            claw.closeClaw();
+            targetState = TurretPosition.INITIAL;
         } else if (controls.doSwivelNorth()) {
-            turret.moveTo(turret.getPosition().withSwivel(0), 0.5);
+            targetState = turret.getPosition().withSwivel(0);
         } else if (controls.doSwivelEast()) {
-            turret.moveTo(turret.getPosition().withSwivel(90), 0.5);
+            targetState = turret.getPosition().withSwivel(90);
         } else if (controls.doSwivelSouth()) {
             if(turret.getPosition().swivelAngle() < 0) {
-                turret.moveTo(turret.getPosition().withSwivel(-180), 0.5);
+                targetState = turret.getPosition().withSwivel(-180);
             } else {
-                turret.moveTo(turret.getPosition().withSwivel(180), 0.5);
+                targetState = turret.getPosition().withSwivel(180);
             }
         } else if (controls.doSwivelWest()) {
-            turret.moveTo(turret.getPosition().withSwivel(-90), 0.5);
+            targetState = turret.getPosition().withSwivel(-90);
         } else if (controls.doAutoPickup()) {
-            turret.moveTo(TurretPosition.INTAKE.withSwivel(turret.getPosition().swivelAngle()));
+            targetState = TurretPosition.INTAKE.withSwivel(turret.getPosition().swivelAngle());
         } else if (controls.doPlayerStation()) {
             claw.openClaw();
-            turret.moveTo(TurretPosition.PLAYER_STATION.withSwivel(turret.getPosition().swivelAngle()));
+            targetState = TurretPosition.PLAYER_STATION.withSwivel(turret.getPosition().swivelAngle());
         // Preset positions are done from the perspective of the driver
-        } else if (controls.getPreset() == 0) {
-            if(controls.doAutoHigh()) {
-                turret.moveTo(TurretPosition.HIGH_MID);
-            } else if (controls.doAutoMid()) {
-                turret.moveTo(TurretPosition.MID_MID);
-            } else {
+        } else if(controls.doAutoHigh()) {
+            targetState = TurretPosition.HIGH_MID;
+        } else if (controls.doAutoMid()) {
+            targetState = TurretPosition.MID_MID;
+        } else if(controls.doAutoHighRight()) {
+            targetState = TurretPosition.HIGH_RIGHT;
+        } else if(controls.doAutoMidRight()) {
+            targetState = TurretPosition.MID_RIGHT;
+        } else if(controls.doAutoHighLeft()) {
+            targetState = TurretPosition.HIGH_LEFT;
+        } else if(controls.doAutoMidLeft()) {
+            targetState = TurretPosition.MID_LEFT; 
+        } else {
+            if(Math.abs(controls.getArmPivot()) > 0.1 || Math.abs(controls.getArmSwivel()) > 0.1 || Math.abs(controls.getArmExtend()) > 0.1) {
+                targetState = null;
                 if(controls.doUnlockSwivel()) {
                     turret.move(controls.getArmPivot(), controls.getArmSwivel(), controls.getArmExtend());
                 } else {
                     turret.move(controls.getArmPivot(), 0, controls.getArmExtend());
+                }          
+            } else {
+                if(targetState!=null) {
+                    turret.moveTo(targetState);
+                } else {
+                    turret.stop();
                 }
-                
             }
-        } else if (controls.getPreset() == 1) {
-            if(controls.doAutoHigh()) {
-                turret.moveTo(TurretPosition.HIGH_RIGHT);
-            } else if(controls.doAutoMid()) {
-                turret.moveTo(TurretPosition.MID_RIGHT);
-            }
-        } else if (controls.getPreset() == -1) {
-            if(controls.doAutoHigh()) {
-                turret.moveTo(TurretPosition.HIGH_LEFT);
-            } else if(controls.doAutoMid()) {
-                turret.moveTo(TurretPosition.MID_LEFT);
-            } 
-        } else {
-            turret.stop();
         }
-
+        
         if (controls.openClaw()) {
             claw.openClaw();
         } else if (controls.closeClaw()) {
             claw.closeClaw();
+        } else if (controls.toggleClaw()) {
+            claw.toggleClaw();
         }
+
         // PUT DEBUG STATEMENTS HERE
         SmartDashboard.putNumber("Pivot.position", Subsystems.turret.getPosition().pivotAngle());
         SmartDashboard.putNumber("Swivel.position", Subsystems.turret.getPosition().swivelAngle());
